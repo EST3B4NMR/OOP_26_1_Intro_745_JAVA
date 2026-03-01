a314e9ccc377b814fe4357047203fe804ad22802
Kiloster <estebanalejandrom@gmail.com>
2026-03-01 12:48:27 -0500

    Implementados elementomenor

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -140,7 +140,12 @@ if (arreglo.length == 0) return 0;
     public int encontrarElementoMenor(int[] arreglo) {
         // TODO: Implementar el método para encontrar el elemento menor en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 1.
-        return 0;
+if (arreglo.length == 0) return 0;
+        int menor = arreglo[0];
+        for (int n : arreglo) {
+            if (n < menor) menor = n;
+        }
+        return menor;
     }
 
     // Método que busca un elemento en un arreglo
